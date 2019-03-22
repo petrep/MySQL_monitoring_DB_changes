2c313414f9731f838bfcafeeb62364c01ad1b939
peterpetrekanics <peter.petrekanics@liferay.com>
2019-03-22 14:42:15 +0100

    Working version with newer binlog connector

--- readme.txt
+++ readme.txt
@@ -9,6 +9,7 @@ https://github.com/shyiko/mysql-binlog-connector-java
 
 
 This tool will use the MySQL binary log file to read DB changes.
+(Tested with MySQL version: 14.14 Distrib 5.7.25)
 
 To set up the binary log file:
 sudo gedit /etc/my.cnf   *UPDATE:  the correct command is:   sudo gedit /etc/mysql/mysql.conf.d/mysqld.cnf
@@ -24,8 +25,11 @@ sudo gedit /etc/my.cnf   *UPDATE:  the correct command is:   sudo gedit /etc/mys
  and restart the server to apply the my.cnf changes:
   sudo /etc/init.d/mysql restart
 
+The app will monitor all the databases handled by that MySQL server.
+
+
 mysql -u root -p
-USE canbedeleted111
+USE somedatabasename
 GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'root'@'localhost';
 show master status;   ->  this file be shown in the results:  mysql-bin.000001
 
--- src/MySQL_monitoring_DB_changes.java
+++ src/MySQL_monitoring_DB_changes.java
@@ -1,33 +1,80 @@
 import java.io.IOException;
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import com.github.shyiko.mysql.binlog.BinaryLogClient;
 import com.github.shyiko.mysql.binlog.event.EventData;
 import com.github.shyiko.mysql.binlog.event.TableMapEventData;
+import com.github.shyiko.mysql.binlog.event.TableMapEventMetadata;
+import com.github.shyiko.mysql.binlog.event.UpdateRowsEventData;
 
 public class MySQL_monitoring_DB_changes {
+
+	static String driver;
+	static String url;
+	static String userName;
+	static String password;
+	static String hostname;
+	static Connection conn;
+	static ResultSet rs;
+	static Statement testStatement;
 
 	public static void main(String[] args) throws IOException {
 		System.out.println("Testing starts..");
 
+		String db_name = "70x";
+		// *** MySQL section start ***
+		driver = "com.mysql.jdbc.Driver";
+		hostname = "localhost";
+		url = "jdbc:mysql://localhost:3306/shardc?useUnicode=true&characterEncoding=UTF-8&useFastDataParsing=false";
+		userName = "root";
+		password = "password";
+		// *** MySQL section end ***
+
 		final Map<String, Long> tableMap = new HashMap<String, Long>();
-		BinaryLogClient client =
-	              new BinaryLogClient("localhost", 3306, "root", "password");
+		BinaryLogClient client = new BinaryLogClient(hostname, 3306, userName, password);
 
-        client.registerEventListener(event -> {
-            EventData data = event.getData();
+		client.registerEventListener(event -> {
+			EventData data = event.getData();
 
-            if(data instanceof TableMapEventData) {
-                TableMapEventData tableData = (TableMapEventData)data;
-                tableMap.put(tableData.getTable(), tableData.getTableId());
-                System.out.println("tableData" + tableData.toString());
-            }
-        });
-        client.connect();
-    
-		
+			if (data instanceof TableMapEventData) {
+				TableMapEventData tableData = (TableMapEventData) data;
+				String tableData_dataBase = tableData.getDatabase();
+				tableMap.put(tableData.getTable(), tableData.getTableId());
+				System.out.println("General information about a table was requested: " + tableData.toString());
+			} else if (data instanceof UpdateRowsEventData) {
+				UpdateRowsEventData eventData = (UpdateRowsEventData) data;
+				TableMapEventMetadata my = new TableMapEventMetadata();
+
+				long updatedTableName = eventData.getTableId();
+
+				for (Entry<Serializable[], Serializable[]> row : eventData.getRows()) {
+					{
+						System.out.println("The following db table row was updated: " + getProductMap(db_name, row.getValue()));
+					}
+				}
+			}
+
+		});
+		client.connect();
 		System.out.println("Testing ends..");
+	}
+
+	static Map<String, String> getProductMap(String db_name, Object[] product) {
+		Map<String, String> map = new HashMap<>();
+		int tableColumnCount = product.length;
+
+		for (int i = 0; i < tableColumnCount; i++) {
+			int columnCount = i + 1;
+			map.put("column" + columnCount, java.lang.String.valueOf(product[i]));
+		}
+
+		return map;
 	}
 
 }
